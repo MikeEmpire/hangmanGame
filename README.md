3b3ea46d05fb8d730c5c01b2be6c526515cdc672
Mike <aolie94@gmail.com>
2017-02-13 15:26:50 -0800

    second commit: created javascript functions and updated README as well

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # Hangman Game
 Hangman game written with HTML, CSS, and Javascript. Game will read users input on the keyboard, and will also allow user to click letter to play the game
 
+## Requirements for the Game
+1. The game will have a word bank to choose from and will be selected at random
+2. There will be an onscreen represenation of the game being played
+3. All guessed letters will be displayed on the screen 
+4. The player doesn't receive multiple mistakes for making the same mistake
+5. Only alphabetical numbers are available
 ## Reason
-This is for my UCLA coding bootcamp class week 3.
-
+This is for my UCLA coding bootcamp class homework week 3.
+## Steps
+1. Initialize the files
+2. Initialize the github
+3. Post github
+4. Create areas for HTML input for the game
+5. Declare Javascript functions
+6. 
+#### Javascript Steps
+1. Declare array for all possible words
+2. 
 ## How to View
 Go to main page:www.github.com/ and download the file. Open the index.html in an updated browser
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,107 @@
-window.onload = function () {
+var wordlist = [],
+		targetWord = '',
+		guesses = [];
+		maxLives = 6;
 
-  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+function loadWordlist() {
+	var word = '';
+	$.ajax({
+		url: 'file://MacintoshHD/Users/afamolie/hangmanGame/assets/JSON/wordlist.json'
+	}).done(function(data) {
+		for (word in data) {
+			wordlist.push(data[word]);
+		}
+	}, 'json');
+}
+
+function endGameDialog(isWinner) {
+	if (isWinner) {
+		$('#endGameDialogTitle').html('You Won');
+		$('#endGameDialogContent').html('You guessed ' + targetWord + guesses.length + 'attempts');
+	} else {
+		$('#endGameDialogTitle').html('You lost');
+		$('#endGameDialogContent').html('Unlucky, The word was ' + targetWord);
+	}
+}
+
+function newWord() {
+	// Select a random word from the generated word list
+	targetWord = wordlist[Math.floor(Math.random() * wordlist.length)];
+}
+
+function cleanGuess() {
+	// To make sure players aren't penalized for guessing the same guess twice
+	var uniqueGuesses = [];
+	$.each(guesses, function(index, element) {
+		if (element.length > 0 && $.inArray(element, uniqueGuesses) == -1) {
+			uniqueGuesses.push(element);
+		}
+	});
+	// override guesses with uniqueGuesses
+	guesses = uniqueGuesses;
+}
+function reviewLives() {
+	var livesRemaining = maxLives,
+			string = targetWord.toLowerCase();
+	for (var i = 0; i<guesses.length; i++) {
+		if (string.indexOf(guesses[i], 0) == -1) {
+			livesRemaining--;
+		}
+	}
+
+	if (livesRemaining <= 0) {
+		// code for end game image
+		setImage(0);
+		endGameDialog(false);
+		return;
+	}
+	// display current number of lives
+	setImage(maxLives - livesRemaining);
+}
+
+function checkIfWon() {
+	if (obfuscateWord() == targetWord) {
+		endGameDialog(true);
+	}
+}
+
+function addGuess() {
+	// take  the element directly from itself rather than traversing through the DOM
+	if(/^[a-zA-Z]*$/.test($('#guess').val()) && typeof $('#guess').val() !== "undefined") {
+		guesses.push($('#guess').val().toLowerCase());
+	}
+}
+function drawWord() {
+	// if there is no targetWord, generate one
+	while (targetWord == '') {
+		newWord();
+	}
+	document.querySelector("#targetWord").innerHTML = (targetWord);
+}
+
+function resetGame() {
+	setImage();
+	targetWord == '';
+	guesses == [];
+	newWord();
+}
+
+function drawGuesses() {
+	guesses.sort();
+	$('#previousGuesses').html(guesses.join(', '));
 
 }
+
+function update() {
+	addGuess();
+	cleanGuess();
+	drawWord();
+	drawGuesses();
+	checkIfWon();
+}
+$(function() {
+	loadWordlist();
+	drawWord();
+	drawGuesses();
+	$('#guess').attr('onkeyup', 'update();');
+});
